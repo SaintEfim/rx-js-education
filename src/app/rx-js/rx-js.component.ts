de4ca7711c4cabd56cc380b34640f7a99f223cc7
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2023-04-21 10:48:54 +0300

    Update rx-js.component.ts

--- src/app/rx-js/rx-js.component.ts
+++ src/app/rx-js/rx-js.component.ts
@@ -1,40 +1,50 @@
+// Импортируем необходимые модули и функции из библиотек
 import { AfterViewInit, Component } from '@angular/core';
-import './rx-js-code' // импортируется внешний код из файла rx-js-code.js
-import { Observable, Subject } from 'rxjs'; // импортируются классы Observable и Subject из пакета rxjs
+import './rx-js-code'; // Импортируем дополнительный код из другого файла
+import { Observable, debounceTime, distinctUntilChanged, map } from 'rxjs'; // Импортируем функции и классы из RxJS
 
 @Component({
-  selector: 'app-rx-js', // селектор компонента
-  templateUrl: './rx-js.component.html', // шаблон компонента
-  styleUrls: ['./rx-js.component.css'] // стили компонента
+  selector: 'app-rx-js', // Селектор, который определяет, какой элемент DOM будет управлять компонентом
+  templateUrl: './rx-js.component.html', // Шаблон компонента
+  styleUrls: ['./rx-js.component.css'] // Стили для компонента
 })
-// Комментарий к первому решению
-// export class RxJsComponent implements AfterViewInit {
-//   ngAfterViewInit(): void {
-//     const val = new Subject(); // создается новый Subject
 
-//     const search=document.getElementById('search'); // получение элемента с id="search"
-//     search?.addEventListener('input',event=>{ // добавление слушателя на событие ввода данных в поле ввода
-//       val.next(event); // передача события в Subject
-//     })
-
-//     val.subscribe(value=>{ // подписка на значения Subject
-//       console.log(value) // вывод каждого введенного символа в консоль
-//     })
-//   }
-// }
-
-// Комментарий ко второму решению
-export class RxJsComponent implements AfterViewInit{
+// Компонент RxJsComponent, который будет отображать информацию по RXJS
+export class RxJsComponent implements AfterViewInit {
   ngAfterViewInit(): void {
-    const search$=new Observable(observer=>{ // создание нового Observable
-        const search=document.getElementById('search'); // получение элемента с id="search"
-        search?.addEventListener('input',event=>{ // добавление слушателя на событие ввода данных в поле ввода
-          observer.next(event); // передача события в Observable
-        })
-    })
+    // Создаем новый Observable (наблюдаемый объект) search$ и передаем ему функцию обратного вызова (observer)
+    const search$ = new Observable<Event>(observer => {
+      // Находим элемент input с идентификатором 'search'
+      const search = document.getElementById('search');
+      if (!search) {
+        observer.error('not find'); // Если элемент не найден, то вызываем ошибку
+        return;
+      }
+      search?.addEventListener('input', event => {
+        observer.next(event); // Когда происходит событие ввода, мы вызываем метод next нашего наблюдаемого объекта
+        // observer.complete(); // Вызываем метод complete, если хотим завершить наблюдаемый объект
+      });
+    });
 
-    search$.subscribe(value=>{ // подписка на значения Observable
-      console.log(value); // вывод каждого введенного символа в консоль
-    })
+    // Используем операторы RxJS для обработки событий ввода
+    search$
+      .pipe(
+        map(event => {
+          return (event.target as HTMLInputElement).value; // Извлекаем значение из события ввода
+        }),
+        debounceTime(500), // Игнорируем события ввода, которые происходят быстрее, чем раз в 500 миллисекунд
+        distinctUntilChanged() // Игнорируем повторяющиеся значения
+      )
+      .subscribe({
+        next: value => {
+          console.log(value); // Выводим значения в консоль
+        },
+        error: err => {
+          console.log(err); // Обрабатываем ошибки
+        },
+        complete: () => {
+          console.log('finish'); // Вызываем, когда наблюдаемый объект завершен
+        }
+      });
   }
 }
